e7138c82baa212987446e67634fecc98da30576d
Andy Bulka <abulka@gmail.com>
2021-07-03 17:58:01 +1000

    import wx

--- example_app.py
+++ example_app.py
@@ -1,6 +1,8 @@
 import requests
+import wx
 
-print("I am example_app")
-for i in range(1000):
+print("I am example_app with wxpython import")
+print(wx.version())
+for i in range(10):
     print(i, end=' ')
 print('done')
